f09fabfcc98f89edeb7c47aad7f043e6b5ed58d2
Matej Taborsky <matej.taborsky@idea-loop.com>
2018-05-11 09:55:43 +0200

    Data persisted using UserDefaults method

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray: [String] = []
+    
+    let defaults = UserDefaults.standard
     
     @IBOutlet weak var addNewItem: UIBarButtonItem!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
+        
     }
     
-    // MARK: Tableview Datasource Methods
+    // MARK: - Tableview Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -34,14 +41,19 @@ class TodoListViewController: UITableViewController {
     }
     
     
-    //MARK - TableView Delegate Methods
+    //MARK: - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        let selectedCell: UITableViewCell = tableView.cellForRow(at: indexPath)!
+        
+        if selectedCell.accessoryType == .checkmark {
+            selectedCell.accessoryType = .none
+            selectedCell.backgroundColor = UIColor.white
+
         } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+            selectedCell.accessoryType = .checkmark
+            selectedCell.backgroundColor = UIColor.init(red: 0, green: 0, blue: 150, alpha: 0.05)
         }
         
         tableView.deselectRow(at: indexPath, animated: true)
@@ -49,7 +61,7 @@ class TodoListViewController: UITableViewController {
     
     
     
-    // MARK: Add new Items
+    // MARK: - Add new Items
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -59,6 +71,9 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
             self.tableView.reloadData()
         }
         
